8fd5fe52affe5b141bf29c511d8564beab99573e
Sammy Nave <sammy.nave@meyouhealth.com>
2015-10-31 14:09:33 -0400

    move create out of component and to route. delete empty model when abandoning post.new

--- front-end/app/pods/posts/new/route.js
+++ front-end/app/pods/posts/new/route.js
@@ -4,5 +4,21 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model(){
     return this.store.createRecord('post');
+  },
+  deactivate(){
+    const post = this.controller.get('post');
+    if(Ember.isNone(post.get('id'))) {
+      post.deleteRecord();
+    }
+  },
+  actions: {
+    createPost(post){
+      const { title, body } = post.getProperties('title', 'body');
+      post.save().then((response) => {
+        this.transitionTo('post', post)
+      }.bind(this)).catch((reason) => {
+        this.set('errorMessage', reason.error);
+      });
+    }
   }
 });
